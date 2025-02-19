cf2ac8122ef9b33bd333cf666e1a694bcda4d0df
luiggix <luiggix@gmail.com>
2025-02-19 20:25:04 +0000

    ver 1

--- nbg/HeCompA/nbgrader_config.py
+++
@@ -1,9 +0,0 @@
-c = get_config() 
-
-########################################
-# MACTI: nbgrader_config.py 
-########################################
-c.CourseDirectory.course_id = "HeCompA" 
-c.IncludeHeaderFooter.header = "source/header.ipynb" 
-c.Exchange.root = "/usr/local/share/nbgrader/exchange" 
-c.CourseDirectory.root = "/home/jovyan/HeCompA/nbg/HeCompA" 
